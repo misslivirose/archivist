2e50194dd6a634ff4c7e98496a8d3df8f06ab629
Liv <oerickson@mozilla.com>
2025-04-23 10:02:52 -0400

    change parse_html to parse_messages

--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,10 +1,10 @@
 mod config;
 mod extract_year;
-mod parse_html;
+mod parse_messages;
 
 pub use config::load_config;
 pub use config::save_config;
 pub use config::AppConfig;
 pub use extract_year::extract_year;
-pub use parse_html::parse_inbox;
-pub use parse_html::parse_timeline;
+pub use parse_messages::parse_inbox;
+pub use parse_messages::parse_timeline;
